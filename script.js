fd68adfafe12a83f0627011e5cbacb3dd8090ca4
Aseem Amitav Dash <91455585+aseemdash@users.noreply.github.com>
2022-11-29 23:29:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+'use script';
+
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+const search = document.querySelector('.search');
+
+btn.addEventListener('click', function () {
+  search.classList.toggle('active');
+  input.focus();
+});
